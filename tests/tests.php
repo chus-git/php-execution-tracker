cf0e5f44b8441d255a003e0caafd08f40fb87896
Jesús Lorenzo Gallego <jelorenzogal@gmail.com>
2024-01-20 16:06:26 +0100

    add test

--- tests/tests.php
+++ tests/tests.php
@@ -1,4 +1,12 @@
 <?php
 
-require_once "./src/ExecutionTracker.php";
+require_once __DIR__ . "/../src/ExecutionTracker/Tracker.php";
+
+use ExecutionTracker\Tracker;
+
+$procedure = Tracker::beginProcedure("Main procedure");
+
+$procedure->end("a");
+
+echo $procedure->asJson();
 
